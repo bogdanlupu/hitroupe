b365e7928a153004fa68539b9b9bb709404b7a2c
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-03-10 18:13:48 +0200

    Styling search box

--- hitroupe-frontend/src/app/components/search/search.component.ts
+++ hitroupe-frontend/src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
 
 @Component({
   selector: 'app-search',
