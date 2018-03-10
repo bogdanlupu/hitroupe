b4bdc393597e531ded64e231953872bf05d53658
Bogdan Lupu <bogdanlupu13@yahoo.ro>
2018-03-10 13:01:20 +0200

    Initial commit

---
+++ hitroupe-backend/src/main/java/com/innovationlabs/hitroupe/HitroupeApplication.java
@@ -0,0 +1,12 @@
+package com.innovationlabs.hitroupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitroupeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitroupeApplication.class, args);
+	}
+}
---
+++ hitroupe-backend/src/test/java/com/innovationlabs/hitroupe/HitroupeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.innovationlabs.hitroupe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HitroupeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
