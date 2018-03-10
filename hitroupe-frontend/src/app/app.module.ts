7900380b4c95508341e890fcd2ee2fdc83854b0a
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-03-10 16:52:50 +0200

    Frontend start

--- hitroupe-frontend/src/app/app.module.ts
+++ hitroupe-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import {AppRoutingModule} from './routing/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ hitroupe-frontend/src/app/routing/app-routing.module.ts
@@ -0,0 +1,15 @@
+import {RouterModule, Routes} from '@angular/router';
+import {SearchComponent} from '../components/search/search.component';
+import {NgModule} from '@angular/core';
+
+const routes: Routes = [
+  {path: '', component: SearchComponent}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
